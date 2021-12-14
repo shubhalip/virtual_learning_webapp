e5c526b36e57cdce0ed3612e01e3a082e3ed9a13
shubhali_kadam <shubhali_kadam@HJNZL001992.persistent.co.in>
2021-12-14 19:54:47 +0530

    First Commit

---
+++ virtual_learning/src/main/java/com/psl/training/VirtualLearningApplication.java
@@ -0,0 +1,13 @@
+package com.psl.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualLearningApplication.class, args);
+	}
+
+}
