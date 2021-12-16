03d91bfce6ddc409d95d3db0e9e2b2c054bec92a
samartha_babar <samartha_babar@ARPGL006647.persistent.co.in>
2021-12-16 20:17:51 +0530

    User repo

--- virtual_learning/src/main/java/com/virtuallearning/backend/repository/UserRepository.java
+++ virtual_learning/src/main/java/com/virtuallearning/backend/repository/UserRepository.java
@@ -1,5 +1,13 @@
 package com.virtuallearning.backend.repository;
 
-public class UserRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.virtuallearning.backend.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+	List<User> findByNameContaining(String name);
 }
