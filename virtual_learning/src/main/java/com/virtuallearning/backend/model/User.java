4b848a726fc231bf2f218d7494ecaa2f0858ffb1
samartha_babar <samartha_babar@ARPGL006647.persistent.co.in>
2021-12-16 20:07:26 +0530

    User Model

--- virtual_learning/src/main/java/com/virtuallearning/backend/model/User.java
+++ virtual_learning/src/main/java/com/virtuallearning/backend/model/User.java
@@ -1,5 +1,119 @@
 package com.virtuallearning.backend.model;
 
-public class User {
+import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="users")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "adress")
+	private String adress;
+	
+	@Column(name = "phone")
+	private String phone;
+	
+	@Column(name = "email")
+	private String email;
+	
+	@Column(name = "password")
+	private String password;
+	
+	@Column(name = "upload_photo", columnDefinition = "TEXT")
+	private String uploadPhoto;
+	
+	@Column(name = "register_date")
+	private LocalDate registerDate;
+	
+	public User() {
+	}
+	
+	public User(String name, String adress, String phone, String email, String password, String uploadPhoto, LocalDate registerDate) {
+		super();
+		this.name = name;
+		this.adress = adress;
+		this.phone = phone;
+		this.email = email;
+		this.password = password;
+		this.uploadPhoto = uploadPhoto;
+		this.registerDate = registerDate;
+	}
+	
+	public long getId() {
+		return id;
+	}
+	
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getAdress() {
+		return adress;
+	}
+	
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+	
+	public String getPhone() {
+		return phone;
+	}
+	
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getUploadPhoto() {
+		return uploadPhoto;
+	}
+	
+	public void setUploadPhoto(String uploadPhoto) {
+		this.uploadPhoto = uploadPhoto;
+	}
+	
+	public LocalDate getRegisterDate() {
+		return registerDate;
+	}
+	
+	public void setRegisterDate(LocalDate registerDate) {
+		this.registerDate = registerDate;
+	}
+	
 }
